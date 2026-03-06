b386ccbdc2d62cfd06c3f03461d4f884cbfad09f
jongibb3-oss <jongibb3@gmail.com>
2026-03-05 18:57:48 -1000

    Add RootLayout component and metadata for app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Kauai Permit Prescreen",
+  description: "County of Kauai Permit Pre-Screen Program",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
